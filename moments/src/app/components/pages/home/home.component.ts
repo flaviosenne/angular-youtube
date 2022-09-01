21592e482c9a7f084516db462d0219b8291b03f8
Flavio <flaviosenne123@gmail.com>
2022-09-01 10:11:04 -0300

    criar serviço para buscar momentos e mostrar no html

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = []
+  moments: Moment[] = []
+  baseAPiUrl = environment.baseApiUrl
 
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
+    this.momentService.getMoments().subscribe(items => {
+      const {data} = items
+
+      data.map(item =>{
+        item.created_at = new Date(item.created_at!).toLocaleTimeString('pt-BR')
+      })
+      
+      this.allMoments = data
+      this.moments = data
+    })
   }
 
 }
--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Moment} from '../Moment'
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class MomentService {
 
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData)
+  }
+
+  getMoments(): Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
   }
 }
